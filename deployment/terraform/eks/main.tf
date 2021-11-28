5e0fab341e5eee21ae33e7bbcd5f6ae5546638ec
edersonbrilhante <contato@edersonbrilhante.com.br>
2021-11-28 15:28:28 +0100

    chore: move eks tf scripts to new path

---
+++ deployment/terraform/eks/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = var.region
+  
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
---
+++ deployment/terraform/eks/variables.tf
@@ -0,0 +1,9 @@
+variable "eks_cluster_name" {
+    type = string
+}
+variable "eks_cluster_version" {
+    type = string
+}
+variable "region" {
+    type = string
+}
