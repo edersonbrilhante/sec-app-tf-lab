8c2267aa7b7a2fae19b1029ba64c2946c29245e7
edersonbrilhante <contato@edersonbrilhante.com.br>
2021-11-28 15:34:29 +0100

    refactor: delete old tf files

--- deployment/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "eks_cluster_name" {
-    type = string
-}
-variable "eks_cluster_version" {
-    type = string
-}
-variable "region" {
-    type = string
-}
--- deployment/terraform/variables.tfvars
+++
@@ -1,3 +0,0 @@
-eks_cluster_version = "1.21"
-eks_cluster_name    = "my-cluster"
-region = "us-west-1"
